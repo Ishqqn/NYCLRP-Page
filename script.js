61ec4b41ff7a35409da4f288486b3949798e8b2d
Ishqqn <iszaanszan@gmail.com>
2026-09-26 11:53:10 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function copyServerCode() {
-    const code = document.getElementById("serverCode");
-    if (!code) return;
-    navigator.clipboard.writeText(code.innerText).then(() => {
-        alert("Server code copied!");
-    });
-}
